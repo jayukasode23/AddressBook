861c7b0ef8e345b667983c96fccbf1289b39bb18
jayukasode23 <jayukasode2398@gmail.com>
2022-10-13 20:44:34 +0530

    Added AddressBook Program

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+using AddressBookSystem;
+
+Console.WriteLine("Welcome To Address Book Program");
+
+AddressBook obj = new AddressBook("Jayshree", "Kasode", "Makhmalabad Naka", "Nashik", "Maharashtra", 422004, 9998887776, "jayukasode@2398gmail.com");
+obj.DisplayContactDetails();
+Console.ReadKey();
